c1d9e15e4ab7bb4e7cc00f69e81ce70f04a2523e
Vinicios Neves <vinicios.neves@mac>
2024-10-19 16:09:13 +0100

    Video 3

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,29 @@
-import { Stack } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Drawer } from 'expo-router/drawer';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        <Drawer.Screen 
+          name="index"
+          options={{
+            drawerLabel: 'Home'
+          }}
+        />
+        <Drawer.Screen 
+          name="about"
+          options={{
+            drawerLabel: 'Sobre'
+          }}
+        />
+        <Drawer.Screen 
+          name="contact/index"
+          options={{
+            drawerLabel: 'Contato'
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
